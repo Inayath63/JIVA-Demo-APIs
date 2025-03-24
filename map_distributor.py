f5613878af5e270259c9403d160036399e0914f3
Inayath63 <100921015+Inayath63@users.noreply.github.com>
2025-03-24 18:09:38 +0530

    Update map_distributor.py

--- map_distributor.py
+++ map_distributor.py
@@ -37,7 +37,7 @@ def write_csv_to_s3(df, bucket, key):
 @router.post("")
 async def map_distributor():
     """
-    Map distributor URLs from Demo Distributor URLs.csv to Product_sheet_updated.csv
+    Map distributor URLs from Demo Distributor URLs.csv to Product_sheet.csv
     and update the S3 file.
     """
     try:
